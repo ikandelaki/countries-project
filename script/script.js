bc92fab68f2a36d7de35a6c5c205d8cda2218530
kandelseik <iraklikandelaki2@gmail.com>
2022-05-23 14:34:01 +0400

    everything works. the only problem is that api fetches wrong neighbours. weird

--- script/script.js
+++ script/script.js
@@ -1,17 +1,22 @@
 const parentEl = document.querySelector(".container");
 const form = document.querySelector(".form-country");
 
-// Renders list of countries to choose from
+// Renders list of countries to choose from,
+// IF there are multiple countries with similar name
 const chooseCountry = function (countries) {
-  const countryNames = countries.map((country) => {
-    return `<span class="choose-country">${country.name.common}</span>`;
-  });
+  const countryNames = countries
+    .map((country) => {
+      return `<span class="choose-country">${country.name.common}</span>`;
+    })
+    .join(" ");
 
   const html = `
   <div class="choose-box">
     <p class="choose-message">There are ${countries.length} countries to choose from.</p>
     <p class="choose-message">Which one do you want to display?</p>
+    <div>
     ${countryNames}
+    </div>
   </div>
   `;
 
@@ -30,6 +35,7 @@ const chooseCountry = function (countries) {
         if (countryName === count.name.common) {
           parentEl.innerHTML = "";
           renderCountry(count);
+          getNeighboursData(count);
         }
       });
     });
@@ -38,12 +44,12 @@ const chooseCountry = function (countries) {
 
 // Create markup for the country we want to render
 const renderCountry = function (country) {
+  console.log(country);
   // Create constants for future use. we need to store currency and languages in constants
   // because they differ from country to country and it has to be done in order for code
   // to work for every country
   const currency = Object.keys(country.currencies)[0];
   const language = Object.keys(country.languages)[0];
-  console.log(Object.keys(country.currencies));
 
   // Create the html for the country
   const html = `
@@ -63,9 +69,9 @@ const renderCountry = function (country) {
         </div>
         <div class="country-detail">
           <p class="country-currency label">Currency</p>
-          <p class="country-currency">${
-            country.currencies[currency].name
-          } <br> ${country.currencies[currency].symbol}</p>
+          <p class="country-currency">${country.currencies[currency].name} ${
+    country.currencies[currency].symbol
+  }</p>
         </div>
         <div class="country-detail">
           <p class="country-languages label">Language</p>
@@ -82,6 +88,7 @@ const renderCountry = function (country) {
   `;
   // Insert generated markup into the parent element
   parentEl.insertAdjacentHTML("afterbegin", html);
+  parentEl.style.opacity = 1;
 };
 
 // Get data about the country
@@ -102,12 +109,76 @@ const getCountryData = async function (country) {
     } else {
       // If there's only one country, render it
       renderCountry(data[0]);
+      getNeighboursData(data[0]);
     }
 
     // Render errors that show up
   } catch (err) {
     const error = `<p class="error-message"> ${err} </p>`;
     parentEl.insertAdjacentHTML("afterbegin", error);
+  }
+};
+
+const renderNeighbours = function (neighbours) {
+  console.log(neighbours);
+  const currency = Object.keys(neighbours.currencies)[0];
+  const language = Object.keys(neighbours.languages)[0];
+
+  html = `
+      <div class="neighbour">
+        <div class="neighbour-image-box">
+          <img
+            class="neighbour-image"
+            src="${neighbours.flags.png}"
+            alt="neighbour image"
+          />
+        </div>
+        <span class="neighbour-label">Neighbour</span>
+        <div class="neighbour-information">
+          <p class="neighbour-name">${neighbours.name.common}</p>
+          <div class="neighbour-detail">
+            <p class="neighbour-capital label">Capital</p>
+            <p class="neighbour-capital">${neighbours.capital[0]}</p>
+          </div>
+          <div class="neighbour-detail">
+            <p class="neighbour-currency label">Currency</p>
+            <p class="neighbour-currency">${
+              neighbours.currencies[currency].name
+            } ${neighbours.currencies[currency].symbol}</p>
+          </div>
+          <div class="neighbour-detail">
+            <p class="neighbour-languages label">Languages</p>
+            <p class="neighbour-languages">${neighbours.languages[language]}</p>
+          </div>
+          <div class="neighbour-detail">
+            <p class="neighbour-neighbour label">Population</p>
+            <p class="neighbour-neighbour">${Math.ceil(
+              neighbours.population / 1000000
+            )} mil</p>
+          </div>
+        </div>
+      </div>
+    `;
+  parentEl.insertAdjacentHTML("beforeend", html);
+};
+
+// Getting the information about neighbours
+const getNeighboursData = async function (neighbours) {
+  try {
+    if (!neighbours.borders) {
+      const html = `<p class="no-neighbours">The country has no neighbours</p>`;
+      parentEl.insertAdjacentHTML("beforeend", html);
+    } else {
+      neighbours.borders.forEach(async function (neighbour) {
+        const res = await fetch(
+          `https://restcountries.com/v3.1/name/${neighbour}`
+        );
+        const [data] = await res.json();
+        renderNeighbours(data);
+      });
+    }
+  } catch (err) {
+    console.log(err);
   }
 };
 
